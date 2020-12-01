154198750e533afcbc8ddd7dd104a31a158db2b7
Alex <allgoodmans571@gmail.com>
2020-12-01 09:32:09 +0300

    fetch updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import Stages from './components/Stages'
+import React, { Component } from 'react';
+import './index.css'
+import Stages from './components/Stages';
+import Loader from './Loader/Loader';
+import Table from './Table/Table';
 
+class App extends Component {
+  state ={
+    isLoading: true,
+    data: [],
+  }
+  async componentDidMount() {
+    const response = await fetch(` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
+    const data = await response.json()
+    // console.log(data)
+    this.setState({
+      isLoading: false,
+      data
+    })
 
-
-
-
-function App() {
-
-
-
-
-
-
-
-
-  return (
-    <div>
-      <Stages />
-    </div>
-  );
+  }
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <Stages />
+        </div>
+      {
+        this.state.isLoading 
+        ? <Loader />
+        : <Table 
+        data={this.state.data}
+        />
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Stages.js
+++ src/components/Stages.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 
 export default function Stages(props) {
@@ -7,7 +6,7 @@ export default function Stages(props) {
 
     async function handleActStages() {
         console.log(document.querySelector('#actStage').value)
-        let data = {actual_stage: document.querySelector('#actStage').value}
+        let data = {actual_stage: Number(document.querySelector('#actStage').value)}
         try {
             await fetch('http://165.22.192.121:3000/actual_stage', {
             method: 'POST',
@@ -21,10 +20,18 @@ export default function Stages(props) {
         }
 
     }
-
-    function handleTotalStages() {
+    
+    async function handleTotalStages() {
+        console.log(document.querySelector('#totalStages').value)
+        let data = {total_stage: Number(document.querySelector('#totalStages').value)}
         try {
-            console.log(document.querySelector('#totalStages').value)
+            await fetch('http://165.22.192.121:3000/total_stage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        body: JSON.stringify(data)
+      });
         } catch (err) {
             console.error(err);
         }
@@ -33,7 +40,7 @@ export default function Stages(props) {
 
 
     return(
-        <div>
+        <div style={{marginBottom: '1rem'}}>
             <div>
         <div className="input-group mb-3" style={{marginBottom: 0, width: '20%'}}>
             <div className="input-group-prepend" >
@@ -42,15 +49,15 @@ export default function Stages(props) {
 
             <input id='totalStages' type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
             <div className="input-group-prepend">
-            <span className="input-group-text" id="inputGroup-sizing-default" >Актальная стадия</span>
+            <span className="input-group-text" id="inputGroup-sizing-default" >Актуальная стадия</span>
             </div>
             <input id='actStage' type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
 
 
             
         </div>
-        <button className='btn-success'onClick={handleActStages}> Отправить Всего стадий </button> <br />
-        <button className='btn-success'onClick={handleTotalStages}> Отправить Актальная стадия </button>
+        <button className='btn-success'onClick={handleTotalStages}> Отправить Всего стадий </button> <br />
+        <button className='btn-success'onClick={() => handleActStages()}> Отправить Актальная стадия </button>
         </div>
 
 
